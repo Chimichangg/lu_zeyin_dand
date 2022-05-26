04de1d2dd1fdedec87888cff79bc92e1e3ca659d
Hephaestus <zeyinlu@Hephaestus.local>
2022-05-26 19:52:42 -0400

    adding background change function

--- script/main.js
+++ script/main.js
@@ -1,4 +1,26 @@
 (() => {
 	// set up the puzzle pieces and boards
 	
+	//collect the buttons at the bottom of the page
+	let bottomNav = document.querySelectorAll("#buttonHolder img"),	gameBoard = document.querySelector(".puzzle-board");
+
+
+	function changeBGImg() {
+		gameBoard.style.backgroundImage =`url(images/backGround${this.dataset.bgref}.jpg)`
+	}
+
+	// add event handling here 
+	bottomNav.forEach(thumb => thumb.addEventListener("click",changeBGImg));
+
+	
+
+
+
+
+
+
+
+
+
+
 })();
